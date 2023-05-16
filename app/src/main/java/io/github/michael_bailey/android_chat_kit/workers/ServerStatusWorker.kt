fedf83ddb2495a5cea32cb4acf7f67c4acf517fe
michael-bailey <mickyb18a@gmail.com>
2023-05-16 18:57:59 +0100

    Added Worker scaffold, for fetching server statuses

---
+++ app/src/main/java/io/github/michael_bailey/android_chat_kit/workers/ServerStatusWorker.kt
@@ -0,0 +1,22 @@
+package io.github.michael_bailey.android_chat_kit.workers
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import io.github.michael_bailey.android_chat_kit.database.AppDatabase
+import io.github.michael_bailey.android_chat_kit.delegates.interfaces.AppDatabaseDelegate
+import io.github.michael_bailey.android_chat_kit.delegates.interfaces.IDatabaseDelegate
+
+class ServerStatusWorker(
+	context: Context,
+	workerParams: WorkerParameters,
+) : Worker(
+	context,
+	workerParams
+), IDatabaseDelegate<AppDatabase> by AppDatabaseDelegate(
+	context = context
+) {
+	override fun doWork(): Result {
+		TODO("Not yet implemented")
+	}
+}
